bbe5b6828e34980b48e6e50e171b47535956d1df
RyanC <ryancanseco12@gmail.com>
2023-07-17 22:24:23 +0800

    feat: real time chat
    Signed-off-by: RyanC <ryancanseco12@gmail.com>

--- components/messages/ChatComponent.jsx
+++ components/messages/ChatComponent.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Typography, TextField, Button, Avatar } from '@mui/material';
 
-const ChatComponent = ({ messages }) => {
+const ChatComponent = ({ messages, handleSendMessage, newMessage, setNewMessage, userId }) => {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
             <Box sx={{ flex: 1, overflow: 'auto' }}>
@@ -12,41 +12,59 @@ const ChatComponent = ({ messages }) => {
                             display: 'flex',
                             alignItems: 'center',
                             mb: 2,
-                            flexDirection: message.isCurrentUser ? 'row-reverse' : 'row',
+                            flexDirection: userId === message.senderId ? 'row-reverse' : 'row',
                         }}
                     >
-                        <Avatar
-                            src={message.profilePicture}
-                            alt="Profile"
-                            style={{
-                                width: 40,
-                                height: 40,
-                                marginRight: 10,
-                                borderRadius: '100%',
-                                order: message.isCurrentUser ? 2 : 1,
-                            }}
-                        />
+                        {userId !== message.senderId && (
+                            <Avatar
+                                src={message.profilePicture}
+                                alt="Profile"
+                                style={{
+                                    width: 40,
+                                    height: 40,
+                                    marginRight: 10,
+                                    borderRadius: '100%',
+                                }}
+                            />
+                        )}
+
                         <Box
                             sx={{
                                 display: 'flex',
                                 maxWidth: '50%',
                                 flexDirection: 'column',
                                 flexGrow: 1,
-                                backgroundColor: message.isCurrentUser ? '#7FFFD4' : '#fff',
+                                backgroundColor: message.senderId === userId ? '#7FFFD4' : '#fff',
                                 color: 'white',
                                 borderRadius: '15px',
                                 padding: '20px',
-                                order: message.isCurrentUser ? 1 : 2,
+                                order: message.senderId === userId ? 1 : 2,
                                 boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
                             }}
                         >
                             <Typography variant="body2" color="textSecondary">
-                                {message.time}
+                                {new Date(message.time).toLocaleString('en-US', {
+                                    hour: 'numeric',
+                                    minute: 'numeric',
+                                    hour12: true,
+                                })}
                             </Typography>
                             <Typography variant="body1" color="textSecondary">
                                 {message.text}
                             </Typography>
                         </Box>
+                        {userId === message.senderId && (
+                            <Avatar
+                                src={message.profilePicture}
+                                alt="Profile"
+                                style={{
+                                    width: 40,
+                                    height: 40,
+                                    marginLeft: 10,
+                                    borderRadius: '100%',
+                                }}
+                            />
+                        )}
                     </Box>
                 ))}
             </Box>
@@ -55,9 +73,23 @@ const ChatComponent = ({ messages }) => {
                     id="outlined-basic"
                     label="Type a message"
                     variant="outlined"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
                     sx={{ mr: 2, flexGrow: 1, boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)', borderRadius: '15px' }}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            e.preventDefault();
+                            handleSendMessage();
+                        }
+                    }}
                 />
-                <Button variant="contained" sx={{ backgroundColor: '#00A36C', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)' }}>Send</Button>
+                <Button
+                    variant="contained"
+                    sx={{ backgroundColor: '#00A36C', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)' }}
+                    onClick={handleSendMessage}
+                >
+                    Send
+                </Button>
             </Box>
         </Box>
     );
--- pages/chat/[id].jsx
+++ pages/chat/[id].jsx
@@ -3,42 +3,69 @@ import ChatComponent from "components/messages/ChatComponent";
 import ChatNavbar from "components/messages/ChatNav";
 import UserListComponent from "components/user/UserListComponent";
 import AuthContext from "contexts/AuthContext";
+import { collection, query, orderBy, onSnapshot, addDoc, where } from "firebase/firestore";
+import { firebaseDB } from "lib/data/firebase";
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 
 export default function ChatPage() {
     const router = useRouter();
     const { id } = router.query;
+    const { user } = useContext(AuthContext);
 
-    // Sample messages data
-    const messages = [
-        {
-            id: 1,
-            profilePicture: 'https://images.pexels.com/photos/17585373/pexels-photo-17585373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            time: '10:30 AM',
-            text: 'Hello there!'
-        },
-        {
-            id: 2,
-            profilePicture: 'https://images.pexels.com/photos/17585373/pexels-photo-17585373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            time: '11:00 AM',
-            text: 'How are you doing?'
-        },
-        {
-            id: 3,
-            profilePicture: 'https://images.pexels.com/photos/17573995/pexels-photo-17573995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1  ',
-            time: '11:30 AM',
-            text: 'I am doing great. Thanks for asking!'
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+
+    useEffect(() => {
+        const messagesCollectionRef = collection(firebaseDB, 'messages');
+        const queryIds = [id, user.uid]
+
+        const messagesQuery = query(
+            messagesCollectionRef,
+            orderBy('time'),
+            where('receiverId', 'in', queryIds),
+            where('senderId', 'in', queryIds)
+        );
+
+        const unsubscribe = onSnapshot(messagesQuery, (snapshot) => {
+            const updatedMessages = snapshot.docs.map((doc) => doc.data());
+            setMessages(updatedMessages);
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+    }, []);
+
+    const handleSendMessage = async (e) => {
+        if (newMessage) {
+            const message = {
+                text: newMessage,
+                time: new Date().toISOString(),
+                senderId: user.uid,
+                receiverId: id,
+            };
+
+            try {
+                const messagesCollectionRef = collection(firebaseDB, 'messages');
+                await addDoc(messagesCollectionRef, message);
+                setNewMessage('');
+            } catch (error) {
+                console.error('Error sending message:', error);
+            }
         }
-    ];
-
+    };
+    console.log('user', user)
     return (
         <Box sx={{ display: 'flex', height: '100vh' }}>
             <UserListComponent />
             <Box sx={{ width: '80%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                 <ChatNavbar userId={id} />
                 <Box sx={{ flex: 1, overflow: 'auto', p: 3 }}>
-                    <ChatComponent messages={messages} />
+                    <ChatComponent messages={messages} handleSendMessage={handleSendMessage}
+                        newMessage={newMessage}
+                        setNewMessage={setNewMessage}
+                        userId={user.uid}
+                    />
                 </Box>
             </Box>
         </Box>
